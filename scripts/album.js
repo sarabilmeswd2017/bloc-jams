d01b8dfb548a6fb3a69a4deab384d95ff4a862cc
Sara Bilmes <saraesrig@gmail.com>
2017-05-30 12:11:51 +0300

    Checkpoint-12-playpause

--- scripts/album.js
+++ scripts/album.js
@@ -46,7 +46,7 @@ var albumSimonandGarfunkel = {
 var createSongRow = function(songNumber, songName, songLength){
 var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -79,6 +79,23 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         if (event.target.parentElement.className === 'album-view-song-item') {
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 
     var albums = [albumPicasso, albumMarconi, albumSimonandGarfunkel];
          var index = 1;
