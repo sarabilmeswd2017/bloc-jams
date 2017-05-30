f5b942ffd52c06162afd24bb64305ef2be824723
Sara Bilmes <saraesrig@gmail.com>
2017-05-30 10:10:46 +0300

    modify album.js and album.css

--- scripts/album.js
+++ scripts/album.js
@@ -33,7 +33,7 @@ var albumSimonandGarfunkel = {
       artist: 'Simon and Garfunkel',
       label: 'EM',
       year: '1970',
-     albumArtUrl: 'assets/images/album_covers/botw_cover.jpg',
+     albumArtUrl: 'assets/images/album_covers/simongarfunkel.png',
       songs: [
           { title: 'Bridge Over Troubled Water', duration: '1:01' },
           { title: 'El Condor Pasa', duration: '5:01' },
@@ -46,30 +46,32 @@ var albumSimonandGarfunkel = {
 var createSongRow = function(songNumber, songName, songLength){
 var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
- 
+
      return template;
     };
-var currentlySelectedAlbum;
-var setCurrentAlbum = function(album) {
-     currentlySelectedAlbum = album;
+
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
+/*var currentlySelectedAlbum;*/
+var setCurrentAlbum = function(album) {
+     /*currentlySelectedAlbum = album;*/
+
+
     albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
-    
+
     albumSongList.innerHTML = '';
-    
+
     for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -77,8 +79,44 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
      setCurrentAlbum(albumPicasso);
-    
-       document.getElementsByClassName('album-cover-art').addEventListener('click', function(event){
+
+
+     var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         if (event.target.parentElement.className === 'album-view-song-item') {
+              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+
+    var albums = [albumPicasso, albumMarconi, albumSimonandGarfunkel];
+         var index = 1;
+      albumImage.addEventListener('click', function(event){
+          setCurrentAlbum(albums[index]);
+          index++;
+          if(index == albums.length){
+              index = 0;
+          }
+          });
+
+  /*  albumImage.addEventListener("click", function(event){
+    for(var i = 1;i < albums.length;i++){
+         setCurrentAlbum(albums[i]);
+    } if(i == albums.length){
+         i = 0;}
+ });*/
+
+    /*   document.getElementsByClassName('album-cover-art').addEventListener('click', function(event){
   if(currentlySelectedAlbum == albumPicasso){
         currentlySelectedAlbum = albumMarconi;
      } else if (currentlySelectedAlbum == albumMarconi){
@@ -86,12 +124,6 @@ window.onload = function() {
      } else if (currentlySelectedAlbum == albumSimonandGarfunkel){
          currentlySelectedAlbum = albumPicasso;
      }
-     
-  } );
+
+  } );*/
     };
-
-
-   
-
-
-  
