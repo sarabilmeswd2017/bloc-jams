6f7f50d8ea0ede37be06b48dd7cb4310621b1ebd
Sara Bilmes <saraesrig@gmail.com>
2017-06-02 15:20:43 +0300

    Assignment-13-playpause:complete

--- scripts/album.js
+++ scripts/album.js
@@ -84,7 +84,7 @@ var setCurrentAlbum = function(album) {
 
 } return findParent; };*/
 
-var findParentByClassName = function(element, targetClass) {
+/*var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
@@ -93,14 +93,23 @@ var findParentByClassName = function(element, targetClass) {
         }
         return currentParent;
     }
-};
-
-/*var findParentByClassName = function(element, targetClass) {
-   var currentParent = element.parentElement;
-   if(currentParent.className[i] !== null){
-
-   }
 };*/
+
+var findParentByClassName = function(element, targetClass) {
+   var currentParent = element.parentElement;
+   if(currentParent  === null){
+   console.log("No parent found");
+ } else if(currentParent.className !== targetClass && currentParent.className === null){
+ console.log("No parent found with that class name");
+}
+else if(element){
+while (currentParent.className !== targetClass && currentParent.className !== null) {
+    currentParent = currentParent.parentElement;
+
+}
+return currentParent;
+}
+};
 
 var getSongItem = function(element) {
     switch (element.className) {
