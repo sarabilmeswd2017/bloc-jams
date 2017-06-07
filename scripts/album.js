1ff3eb23cc6d2d06302664bfe5c7dde08d4979ae
Sara Bilmes <saraesrig@gmail.com>
2017-06-07 10:46:42 +0300

    Checkpoint-17-jquery:complete, playpause does not work now

--- scripts/album.js
+++ scripts/album.js
@@ -52,28 +52,29 @@ var template =
       + '</tr>'
       ;
 
-     return template;
+     return $(template);
     };
 
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 /*var currentlySelectedAlbum;*/
 var setCurrentAlbum = function(album) {
      /*currentlySelectedAlbum = album;*/
 
 
-    albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+      $albumArtist.text(album.artist);
+      $albumReleaseInfo.text(album.year + ' ' + album.label);
+      $albumImage.attr('src', album.albumArtUrl);
 
-    albumSongList.innerHTML = '';
+    $albumSongList.empty();
 
     for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      $albumSongList.append($newRow);
      }
 };
 
@@ -137,7 +138,7 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 
 window.onload = function() {
      setCurrentAlbum(albumPicasso);
-     
+
   songListContainer.addEventListener('mouseover', function(event) {
          // #1
           if (event.target.parentElement.className === 'album-view-song-item') {
